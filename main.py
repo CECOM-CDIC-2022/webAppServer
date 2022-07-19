c09a5a50fb612a9c22257e443395f354e87448ed
yymin1022 <yymin1022@gmail.com>
2022-07-19 23:44:03 +0900

    Update main.py : Cleaned UP

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 from flask import Flask,request,jsonify
 
-@app.route('/soundclf', methods = ['GET','POST'])
-def postJsonHandler_class():
-    if request.method == 'POST':
-        try:
-            return 'F21VDT'
-            
-        except Exception as e:
-            print(e)
-            #print(request.data)
-            return 'error'
-        
-    else:
-        return 'clf: not Post'
+@app.route('/soundclf', methods = ['POST'])
+def soundclf():
+    try:
+        return 'F21VDT'       
+    except Exception as e:
+        print(e)
+        return 'error'
 
-@app.route('/soundval', methods = ['GET','POST'])
-#@inflate
-def postJsonHandler():
-    if request.method == 'POST':
-        try:
-            return 'True'
+@app.route('/soundval', methods = ['POST'])
+def soundval():
+    try:
+        return 'True'
+    except Exception as e:
+        print(e)
+        return 'error'
 
-        except Exception as e:
-            print(e)
-            #print(request.data)
-            return 'error'
-            
-    else:
-        return 'val: not Post'
-
-#여기는 알잘딱깔센
-app.run(host='0.0.0.0', debug=True, port= 9999)
+app.run(host='0.0.0.0', debug=True, port= 8080)
