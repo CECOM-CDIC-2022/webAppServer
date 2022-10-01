ba2f4c82249e8c2b57af992284079a21e0310606
yymin1022 <yymin1022@gmail.com>
2022-10-01 23:02:54 +0900

    Update main.py : ADded Sound Transaction API Function

--- ha-sensor.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,request,jsonify
-
-flaskApp = Flask(__name__)
-
-@flaskApp.route("/test", methods=["POST"])
-def test():
-    return "ZipGaGoSipDa"
-
-flaskApp.run(host="0.0.0.0", debug=True, port= 80)
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask,request,jsonify
 
 flaskApp = Flask(__name__)
 
+@flaskApp.route("/sound", methods=["POST"])
+def test():
+    return "WA_SANS"
+
 @flaskApp.route("/test", methods=["POST"])
 def test():
     return "ZipGaGoSipDa"
