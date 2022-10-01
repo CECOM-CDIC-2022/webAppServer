14d63c0cc120bf1d0ba5a8c1c3f3c39eab1ed144
yymin1022 <yymin1022@gmail.com>
2022-10-01 22:49:04 +0900

    Update main.py : Removed Unnecessary API Function:

--- main.py
+++ main.py
@@ -2,22 +2,6 @@ from flask import Flask,request,jsonify
 
 flaskApp = Flask(__name__)
 
-@flaskApp.route("/soundclf", methods=["POST"])
-def soundclf():
-    try:
-        return "F21VDT"       
-    except Exception as e:
-        print(e)
-        return "error"
-
-@flaskApp.route("/soundval", methods=["POST"])
-def soundval():
-    try:
-        return "True"
-    except Exception as e:
-        print(e)
-        return "error"
-
 @flaskApp.route("/test", methods=["POST"])
 def test():
     return "ZipGaGoSipDa"
